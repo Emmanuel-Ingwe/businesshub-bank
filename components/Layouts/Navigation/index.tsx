24371212567370fd8298f279cce445781c7fb686
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-10-10 23:58:19 +0100

    .

--- components/Layouts/Navigation/index.tsx
+++ components/Layouts/Navigation/index.tsx
@@ -9,7 +9,7 @@ const Navigation = () => {
 
 	return (
 		<>
-			<nav className='flex items-center bg-gray-100 z-50 top-0 w-full h-[72px] border-b border-hr'>
+			<nav className='flex items-center bg-gray-100 z-50 top-0 w-full h-[72px] border-b'>
 				<div className='container flex items-center justify-between'>
 					<div>
 						<Link href='/' passHref>
