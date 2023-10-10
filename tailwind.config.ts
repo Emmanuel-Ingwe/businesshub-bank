ff88c344229b9d3efc23bad60d73facb7150ea28
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-10-10 15:58:31 +0100

    //

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,28 +14,6 @@ module.exports = {
     },
     extend: {
       colors: {
-        gray: {
-          "100": "#150e28",
-          "200": "#140d27",
-          "300": "#100b20",
-          "400": "rgba(255, 255, 255, 0.18)",
-          "500": "rgba(255, 255, 255, 0.01)",
-          "600": "rgba(255, 255, 255, 0.75)",
-        },
-        white: "#fff",
-        hr: "rgba(255, 255, 255, 0.18)",
-        blueviolet: {
-          "100": "#903aff",
-          "200": "rgba(144, 58, 255, 0.12)",
-        },
-        fuchsia: {
-          "100": "#d434fe",
-          "200": "rgba(212, 52, 254, 0.12)",
-        },
-        gradient: {
-          general: 'linear-gradient(270deg,_#903aff,_#d434fe_56.42%,_#ff26b9_99.99%,_#fe34b9)',
-          icon: 'linear-gradient(125.8deg, #9666FF 6.28%, #5645F5 99.52%)',
-        },
         primary: "#212121",
         navbg: "#F5F8FC",
         secondary: "#028789",
