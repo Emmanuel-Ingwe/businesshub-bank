09a52bd8ce00e22c3e9966345a8a125f6cf67baa
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-10-12 16:32:35 +0100

    //

--- components/Landing/Subbody/Benefits/index.tsx
+++ components/Landing/Subbody/Benefits/index.tsx
@@ -8,12 +8,42 @@ const Benefits = (props: Props) => {
   return (
     <>
     <section className="flex">
-      <div className="h h-screen w-[235px] bg-brownn pl-[12px] pt-[26px] items-cente">
-        <div className="border-b border-[#768396] box-border">
+      <div className="h h-screen w-[235px] bg-brownn pl-[16px] pt-[43px]">
+        <div className="border-b border-[#768396] box-border w-[166px]">
             <h2 className="font-semibold text-[13px]">Benefits</h2>
         </div>
-        <div className="">
-
+        <div className="flex mt-[14px]">
+            <div className="">
+                <Image
+                    alt=""
+                    height={14}
+                    width={14}
+                    src="/Images/check.png"
+                />
+            </div>
+            <p className="text-[11px] pl-[7px]">Zero AMF for upto 3 year. (Annual Manintanace Fee)</p>
+        </div>
+        <div className="flex mt-[14px]">
+            <div className="">
+                <Image
+                    alt=""
+                    height={14}
+                    width={14}
+                    src="/Images/check.png"
+                />
+            </div>
+            <p className="text-[11px] pl-[7px]">Zero AMF for upto 3 year. (Annual Manintanace Fee)</p>
+        </div>
+        <div className="flex mt-[14px]">
+            <div className="">
+                <Image
+                    alt=""
+                    height={14}
+                    width={14}
+                    src="/Images/check.png"
+                />
+            </div>
+            <p className="text-[11px] pl-[7px]">Zero AMF for upto 3 year. (Annual Manintanace Fee)</p>
         </div>
       </div>
     </section>
