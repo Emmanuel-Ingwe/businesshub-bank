bceab71b7ff0f7cb4d87f995319663749ca8528d
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-10-12 16:10:53 +0100

    border

--- components/Landing/Subbody/Benefits/index.tsx
+++ components/Landing/Subbody/Benefits/index.tsx
@@ -8,8 +8,13 @@ const Benefits = (props: Props) => {
   return (
     <>
     <section className="flex">
-      <div className="h h-screen w-[235px] bg-brownn pl-[12px] pt-[26px]">
-        <div className=""></div>
+      <div className="h h-screen w-[235px] bg-brownn pl-[12px] pt-[26px] items-cente">
+        <div className="border-b border-[#768396] box-border">
+            <h2 className="font-semibold text-[13px]">Benefits</h2>
+        </div>
+        <div className="">
+
+        </div>
       </div>
     </section>
   </>
