8bcc911e36dc835ba80561fcce17624d0d1caf2b
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-11-13 16:17:58 +0100

    ..

--- components/Landing/Subbody/index.tsx
+++ components/Landing/Subbody/index.tsx
@@ -6,181 +6,181 @@ import Button from '../../global/Button'
 type Props = {}
 
 const index = (props: Props) => {
-    type data = {
-		title: "string"
-	}
+  type data = {
+    title: "string"
+  }
 
-	const data = [
+  const data = [
     {
       title: "Basic Business support",
-		},
+    },
     {
       title: "Custom Business support",
-		},
+    },
     {
       title: "View Status",
-		},
-	  ];
+    },
+  ];
 
-      const dataa = [
-        {
-          title: "Business Name",
-          para0: "Business Name Registration", 
-          para1: "TIN Registration", 
-          para2: "Zero AMF for up to 3 years", 
-          para3: "Search Report", 
-          para4: "Open an Account", 
-          image: "/Images/NGR.png",
-          figure: "23,000",
-          button: <Button label='CHOOSE' />,
-        },
-        {
-          title: "Limited Liability",
-          para0: "Register a Limited Liability Company", 
-          para1: "TIN Registration", 
-          para2: "Zero AMF for up to 3 years", 
-          para3: "Search Report", 
-          para4: "Open an Account", 
-          image: "/Images/NGR.png",
-          figure: "47,000",
-          button: <Button label='CHOOSE' />,
-            },
-        {
-          title: "Incoporrated Trustee",
-          para0: "Business Name Registration", 
-          para1: "TIN Registration", 
-          para2: "Zero AMF for up to 3 years", 
-          para3: "Search Report", 
-          para4: "Open an Account", 
-          image: "/images/NGR.png",
-          figure: "150,000",
-          button: <Button label='CHOOSE' />,
-            },
-          {
-          title: "Incoporrated Trustee",
-          para0: "Business Name Registration", 
-          para1: "TIN Registration", 
-          para2: "Zero AMF for up to 3 years", 
-          para3: "Search Report", 
-          para4: "Open an Account", 
-          image: "/images/NGR.png",
-          figure: "150,000",
-          button: <Button label='CHOOSE' />,
-            },
-          {
-          title: "Incoporrated Trustee",
-          para0: "Business Name Registration", 
-          para1: "TIN Registration", 
-          para2: "Zero AMF for up to 3 years", 
-          para3: "Search Report", 
-          para4: "Open an Account", 
-          image: "/images/NGR.png",
-          figure: "150,000",
-          button: <Button label='CHOOSE' />,
-            },
-          {
-          title: "Incoporrated Trustee",
-          para0: "Business Name Registration", 
-          para1: "TIN Registration", 
-          para2: "Zero AMF for up to 3 years", 
-          para3: "Search Report", 
-          para4: "Open an Account", 
-          image: "/images/NGR.png",
-          figure: "150,000",
-          button: <Button label='CHOOSE' />,
-            },
-          ];
+  const dataa = [
+    {
+      title: "Business Name",
+      para0: "Business Name Registration",
+      para1: "TIN Registration",
+      para2: "Zero AMF for up to 3 years",
+      para3: "Search Report",
+      para4: "Open an Account",
+      image: "/Images/NGR.png",
+      figure: "23,000",
+      button: <Button label='CHOOSE' />,
+    },
+    {
+      title: "Limited Liability",
+      para0: "Register a Limited Liability Company",
+      para1: "TIN Registration",
+      para2: "Zero AMF for up to 3 years",
+      para3: "Search Report",
+      para4: "Open an Account",
+      image: "/Images/NGR.png",
+      figure: "47,000",
+      button: <Button label='CHOOSE' />,
+    },
+    {
+      title: "Incoporrated Trustee",
+      para0: "Business Name Registration",
+      para1: "TIN Registration",
+      para2: "Zero AMF for up to 3 years",
+      para3: "Search Report",
+      para4: "Open an Account",
+      image: "/images/NGR.png",
+      figure: "150,000",
+      button: <Button label='CHOOSE' />,
+    },
+    {
+      title: "Incoporrated Trustee",
+      para0: "Business Name Registration",
+      para1: "TIN Registration",
+      para2: "Zero AMF for up to 3 years",
+      para3: "Search Report",
+      para4: "Open an Account",
+      image: "/images/NGR.png",
+      figure: "150,000",
+      button: <Button label='CHOOSE' />,
+    },
+    {
+      title: "Incoporrated Trustee",
+      para0: "Business Name Registration",
+      para1: "TIN Registration",
+      para2: "Zero AMF for up to 3 years",
+      para3: "Search Report",
+      para4: "Open an Account",
+      image: "/images/NGR.png",
+      figure: "150,000",
+      button: <Button label='CHOOSE' />,
+    },
+    {
+      title: "Incoporrated Trustee",
+      para0: "Business Name Registration",
+      para1: "TIN Registration",
+      para2: "Zero AMF for up to 3 years",
+      para3: "Search Report",
+      para4: "Open an Account",
+      image: "/images/NGR.png",
+      figure: "150,000",
+      button: <Button label='CHOOSE' />,
+    },
+  ];
 
   return (
     <>
-        <section className="flex-col flex lg:flex-row w-screen">
-            <Benefits />
-            <>
-                <section className="flex flex-col lg:m-0 m-[49px] w-[60%]">
-                    <div className="flex border-b w-[90%]">
-                        {data.map((item) => (
-                            <div className="flex items-center justify-center border rounded-tr rounded-tl hover:bg-deepbrown border-t-secondary border-l-secondary border-r-secondary mr-[11px] w-full h-[46px]">
-                                <h1 className=" text-secondary">{item.title}</h1>
-                            </div>
-                        ))}
+      <section className="flex-col flex lg:flex-row w-screen">
+        <Benefits />
+        <>
+          <section className="flex flex-col lg:m-0 m-[49px] w-[60%]">
+            <div className="flex border-b w-[90%]">
+              {data.map((item) => (
+                <div className="flex items-center justify-center border rounded-tr rounded-tl hover:bg-deepbrown border-t-secondary border-l-secondary border-r-secondary mr-[11px] w-full h-[46px]">
+                  <h1 className=" text-secondary">{item.title}</h1>
+                </div>
+              ))}
+            </div>
+
+            <div className="grid gap-10 grid-cols-3 mt-[49px]">
+              {dataa.map((item) => (
+                <div className="pl-[21px] pr-[10px] pt-[21px] rounded-xl pb-[21px] shadow-md">
+                  <div className="justify-between border-b flex items-start">
+                    <h3 className="">{item.title}</h3>
+                    <input
+                      type="checkbox"
+                      className="rounded-full border text-black appearance-none h-4 w-4 border-opacity-0 checked:bg-[#F8B400] checked:text-white checked:border-transparent focus:outline-none active:bg-brown active:text-white"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <div className="flex">
+                      <Image
+                        alt=""
+                        height={14}
+                        width={14}
+                        src="/Images/greencheck.png"
+                      />
+                      <p className=" text-primary text-[10px]">{item.para0}</p>
                     </div>
-                    
-                    <div className="grid gap-10 grid-cols-3 mt-[49px]">
-                      {dataa.map((item) => (
-                        <div className="pl-[21px] pr-[10px] pt-[21px] rounded-xl pb-[21px] shadow-md">
-                          <div className="justify-between border-b flex items-start">
-                            <h3 className="">{item.title}</h3>
-                            <input
-                              type="checkbox"
-                              className="rounded-full border text-black appearance-none h-4 w-4 border-opacity-0 checked:bg-[#F8B400] checked:text-white checked:border-transparent focus:outline-none active:bg-brown active:text-white"
-                            />
-                          </div>
-                          <div className="flex flex-col">
-                            <div className="flex">
-                              <Image
-                                  alt=""
-                                  height={14}
-                                  width={14}
-                                  src="/Images/greencheck.png"
-                              />
-                              <p className=" text-primary text-[10px]">{item.para0}</p>
-                            </div>
-                            <div className="flex">
-                              <Image
-                                  alt=""
-                                  height={14}
-                                  width={14}
-                                  src="/Images/greencheck.png"
-                              />
-                              <p className=" text-primary text-[10px]">{item.para1}</p>
-                            </div>
-                            <div className="flex">
-                              <Image
-                                  alt=""
-                                  height={14}
-                                  width={14}
-                                  src="/Images/greencheck.png"
-                              />
-                              <p className=" text-primary text-[10px]">{item.para2}</p>
-                            </div>
-                            <div className="flex">
-                              <Image
-                                  alt=""
-                                  height={14}
-                                  width={14}
-                                  src="/Images/greencheck.png"
-                              />
-                              <p className=" text-primary text-[10px]">{item.para3}</p>
-                            </div>
-                            <div className="flex">
-                              <Image
-                                  alt=""
-                                  height={14}
-                                  width={14}
-                                  src="/Images/greencheck.png"
-                              />
-                              <p className="text-primary text-[10px]">{item.para4}</p>
-                            </div>
-                          </div>
-                          <div className="grid grid-cols-2">
-                            <div className="flex">
-                              <Image
-                                src={item.image}
-                                alt='img'
-                                width={20}
-                                height={20}
-                               /> {item.figure}
-                            </div>
-                            <div className="">
-                              {item.button}
-                            </div>
-                          </div>
-                        </div>  
-                      ))}
+                    <div className="flex">
+                      <Image
+                        alt=""
+                        height={14}
+                        width={14}
+                        src="/Images/greencheck.png"
+                      />
+                      <p className=" text-primary text-[10px]">{item.para1}</p>
                     </div>
-                </section>
-            </>
-        </section>
+                    <div className="flex">
+                      <Image
+                        alt=""
+                        height={14}
+                        width={14}
+                        src="/Images/greencheck.png"
+                      />
+                      <p className=" text-primary text-[10px]">{item.para2}</p>
+                    </div>
+                    <div className="flex">
+                      <Image
+                        alt=""
+                        height={14}
+                        width={14}
+                        src="/Images/greencheck.png"
+                      />
+                      <p className=" text-primary text-[10px]">{item.para3}</p>
+                    </div>
+                    <div className="flex">
+                      <Image
+                        alt=""
+                        height={14}
+                        width={14}
+                        src="/Images/greencheck.png"
+                      />
+                      <p className="text-primary text-[10px]">{item.para4}</p>
+                    </div>
+                  </div>
+                  <div className="grid grid-cols-2">
+                    <div className="flex">
+                      <Image
+                        src={item.image}
+                        alt='img'
+                        width={20}
+                        height={20}
+                      /> {item.figure}
+                    </div>
+                    <div className="">
+                      {item.button}
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </section>
+        </>
+      </section>
     </>
   )
 }
--- components/global/Button/index.tsx
+++ components/global/Button/index.tsx
@@ -5,12 +5,12 @@ interface ButtonProps {
   className?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({  label, className }) => {
+const Button: React.FC<ButtonProps> = ({ label, className }) => {
   return (
     <button
       className={`bg-secondary flex items-center hover:border-gray-800 justify-center w-[85px] h-[25px] text-white rounded focus:outline-none ${className}`}
     >
-      <p className="text-[9px] font-semibold pr-1">{label}</p>
+      <p className="text-[9.1px] font-semibold pr-1">{label}</p>
       <img src="/images/Arrow.png" width={10} alt="" />
     </button>
   );
