826c2b9488a3ffe7bc6ebb27da189b8dcd9edbf3
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-11-15 23:15:56 +0100

    //

--- components/global/Button/index.tsx
+++ components/global/Button/index.tsx
@@ -10,7 +10,7 @@ const Button: React.FC<ButtonProps> = ({ label, className }) => {
     <button
       className={`bg-secondary flex items-center hover:border-gray-800 justify-center w-[85px] h-[25px] text-white rounded focus:outline-none ${className}`}
     >
-      <p className="text-[9.1px] font-semibold pr-1">{label}</p>
+      <p className="text-[9.12px] font-semibold pr-1">{label}</p>
       <img src="/images/Arrow.png" width={10} alt="" />
     </button>
   );
