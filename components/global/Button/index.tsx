2171689c79685cbce79a10f4e31c5b38a108d557
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-11-16 09:40:57 +0100

    add: func. showimg

--- components/global/Button/index.tsx
+++ components/global/Button/index.tsx
@@ -3,15 +3,19 @@ import React from 'react';
 interface ButtonProps {
   label: string;
   className?: string;
+  showimg: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, className }) => {
+const Button: React.FC<ButtonProps> = ({ label, className, showimg }) => {
   return (
     <button
       className={`bg-secondary flex items-center hover:border-gray-800 justify-center w-[85px] h-[25px] text-white rounded focus:outline-none ${className}`}
     >
       <p className="text-[9.12px] font-semibold pr-1">{label}</p>
-      <img src="/images/Arrow.png" width={10} alt="" />
+      {showimg && (
+        <img src="/images/Arrow.png" width={10} alt="" />
+      )
+      }
     </button>
   );
 };
